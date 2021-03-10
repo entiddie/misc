dcbd5a397f963258bebfa8cac84844168b4a0b87
entity <65078619+entiddie@users.noreply.github.com>
2021-03-10 21:17:14 +0530

    Update discord-pagination.py

--- discord-pagination.py
+++ discord-pagination.py
@@ -52,3 +52,68 @@
             except Exception as e:
                 await ctx.send(e)
                 return
+
+            
+    # Embed version        
+            
+            
+    @commands.command()
+    async def page(self, ctx):
+
+        e1 = discord.Embed(description="page 1")
+        e2 = discord.Embed(description="page 2")
+        e3 = discord.Embed(description="page 3")
+
+
+        pages = [e1, e2, e3]
+
+        buttons = ['◀', '▶', '🗑']
+
+        m = await ctx.send(embed=pages[0])
+
+        for reaction in buttons:
+            await m.add_reaction(reaction)
+
+
+        def check(reaction, user):
+            return user == ctx.message.author and str(reaction.emoji) in buttons
+
+        
+        page_track = 0
+
+
+        while True:
+
+                try:
+
+                    reaction, user = await self.client.wait_for('reaction_add', timeout=15, check=check)
+
+                    if reaction.emoji == buttons[0]:
+                        if page_track != 0:
+                            page_track -= 1
+                            await m.edit(embed=pages[page_track])
+
+                        else:
+                            pass
+                        
+
+                    if reaction.emoji == buttons[1]:
+                        if page_track < len(pages) - 1:
+                            page_track += 1
+                            await m.edit(embed=pages[page_track])
+
+                        else:
+                            pass
+                        
+
+                    elif reaction.emoji == buttons[2]:
+                        await m.clear_reactions()
+                        return
+
+                except asyncio.TimeoutError:
+                    await m.clear_reactions()
+                    return
+
+                except Exception as e:
+                    await ctx.send(e)
+                    return
